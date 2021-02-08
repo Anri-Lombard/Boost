0797b8d969c441cab59791f937f912ad465cef71
Anri Lombard <anri.m.lombard@gmail.com>
2021-02-08 14:59:53 +0200

    Booster_48 (Audio for thrust)

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,17 +23,31 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rigidBody.AddRelativeForce(Vector3.up);
-        }
+        PlaySoundWithThrust();
+
         if (Input.GetKey(KeyCode.A))
         {
-            
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            
+            transform.Rotate(Vector3.back);
+        }
+    }
+
+    private void PlaySoundWithThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
 }
